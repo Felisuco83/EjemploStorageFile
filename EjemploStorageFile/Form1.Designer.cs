2e092eb6652762564ff73ccb19cd2f0dab1afade
Felix Itiel Arino Gila <FelixItiel.Arino@tajamar365.com>
2021-03-08 14:05:10 +0100

    Add project files.

---
+++ EjemploStorageFile/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿
+namespace EjemploStorageFile
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnleerfile = new System.Windows.Forms.Button();
+            this.txtcontenido = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // btnleerfile
+            // 
+            this.btnleerfile.Location = new System.Drawing.Point(25, 28);
+            this.btnleerfile.Name = "btnleerfile";
+            this.btnleerfile.Size = new System.Drawing.Size(175, 103);
+            this.btnleerfile.TabIndex = 0;
+            this.btnleerfile.Text = "Read Azure File";
+            this.btnleerfile.UseVisualStyleBackColor = true;
+            this.btnleerfile.Click += new System.EventHandler(this.btnleerfile_Click);
+            // 
+            // txtcontenido
+            // 
+            this.txtcontenido.Location = new System.Drawing.Point(241, 28);
+            this.txtcontenido.Name = "txtcontenido";
+            this.txtcontenido.Size = new System.Drawing.Size(532, 410);
+            this.txtcontenido.TabIndex = 1;
+            this.txtcontenido.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtcontenido);
+            this.Controls.Add(this.btnleerfile);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnleerfile;
+        private System.Windows.Forms.RichTextBox txtcontenido;
+    }
+}
+
---
+++ EjemploStorageFile/Form1.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Azure;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.File;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EjemploStorageFile
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private async void btnleerfile_Click(object sender, EventArgs e)
+        {
+            //NECESITAMOS LA CLAVE DE ACCESO A STORAGE
+            string key = CloudConfigurationManager.GetSetting("storagecnstring");
+            //NECESITAMOS ACCEDER A TODA LA CUENTA CLOUD STORAGE PARA PODER ACCEDER A FILES
+            CloudStorageAccount account = CloudStorageAccount.Parse(key);
+            CloudFileClient client = account.CreateCloudFileClient();
+            CloudFileShare fileshare = client.GetShareReference("ejemplo");
+            CloudFileDirectory raiz = fileshare.GetRootDirectoryReference();
+            CloudFile file = raiz.GetFileReference("clientessimples.json");
+            string contenido = await file.DownloadTextAsync();
+            this.txtcontenido.Text = contenido;
+        }
+    }
+}
